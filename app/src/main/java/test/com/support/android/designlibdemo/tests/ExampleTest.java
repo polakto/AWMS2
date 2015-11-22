0f6daecbbfd144db3dc36f70eaa7d04222a6ca72
polakto <polakto@gmail.com>
2015-11-22 19:17:21 +0300

    Added testing class. v4.3

--- app/src/main/java/test/com/support/android/designlibdemo/tests/ExampleTest.java
+++ app/src/main/java/test/com/support/android/designlibdemo/tests/ExampleTest.java
@@ -2,10 +2,20 @@ package com.support.android.designlibdemo.tests;
 
 import android.test.InstrumentationTestCase;
 
+import com.support.android.designlibdemo.subjectOfTesting;
+
 /**
  * Created by Dell on 19.11.2015.
  */
 public class ExampleTest extends InstrumentationTestCase {
+    /**
+     * This is template for all your tests. In general, you need to define class, variables or
+     * something different and in the end of this method there need to be method assertEquals(x,y)
+     * where are you comparing the results and then the android studio will show you results of
+     * your test method.
+     *
+     * @throws Exception
+     */
     public void test() throws Exception {
         final int expected = 1;
         final int reality = 5;
@@ -32,7 +42,16 @@ public class ExampleTest extends InstrumentationTestCase {
 
     public void test5() throws Exception {
         final int expected = 6;
-        final int reality = 6;
+        subjectOfTesting testedClass = new subjectOfTesting();
+        int reality = testedClass.sum(6, 9);
+
+        assertEquals(expected, reality);
+    }
+
+    public void test6() throws Exception {
+        final int expected = 15;
+        subjectOfTesting testedClass = new subjectOfTesting();
+        int reality = testedClass.sum(6, 9);
 
         assertEquals(expected, reality);
     }
