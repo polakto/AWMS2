5409a7ce7cda4359ca55c41cebb7c27481294df8
gaurav sharma <sharma25gaurav@gmail.com>
2015-11-22 17:34:43 +0300

    Gaurav's test 3

--- app/src/main/java/test/com/support/android/designlibdemo/tests/ExampleTest.java
+++ app/src/main/java/test/com/support/android/designlibdemo/tests/ExampleTest.java
@@ -6,7 +6,7 @@ import android.test.InstrumentationTestCase;
  * Created by Dell on 19.11.2015.
  * Editacia editacia praca marna!!!
  * Hijhjg
- * Test Gaurav
+ * Test Gaurav - this is next test
  */
 public class ExampleTest extends InstrumentationTestCase {
     public void test() throws Exception {
