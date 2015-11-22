34a36bee73b1aa06376607cd59a5cafe13dfee93
polakto <polakto@gmail.com>
2015-11-22 18:36:37 +0300

    Added testing class. v3

--- app/src/main/java/test/com/support/android/designlibdemo/tests/ExampleTest.java
+++ app/src/main/java/test/com/support/android/designlibdemo/tests/ExampleTest.java
@@ -15,4 +15,28 @@ public class ExampleTest extends InstrumentationTestCase {
         final int reality = 5;
         assertEquals(expected, reality);
     }
+
+    public void test2() throws Exception {
+        final int expected = 2;
+        final int reality = 2;
+        assertEquals(expected, reality);
+    }
+
+    public void test3() throws Exception {
+        final int expected = 3;
+        final int reality = 5;
+        assertEquals(expected, reality);
+    }
+
+    public void test4() throws Exception {
+        final int expected = 1;
+        final int reality = 1;
+        assertEquals(expected, reality);
+    }
+
+    public void test5() throws Exception {
+        final int expected = 6;
+        final int reality = 6;
+        assertEquals(expected, reality);
+    }
 }
