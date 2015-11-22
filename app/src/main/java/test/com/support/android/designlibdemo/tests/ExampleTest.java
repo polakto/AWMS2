15447bc916e8f32998a02b929f6b5d869e8af4f1
polakto <polakto@gmail.com>
2015-11-22 18:48:13 +0300

    Added testing class. v4.1

--- app/src/main/java/test/com/support/android/designlibdemo/tests/ExampleTest.java
+++ app/src/main/java/test/com/support/android/designlibdemo/tests/ExampleTest.java
@@ -4,10 +4,6 @@ import android.test.InstrumentationTestCase;
 
 /**
  * Created by Dell on 19.11.2015.
- * Editacia editacia praca marna!!!
- * Hijhjg
- * Test Gaurav - this is next test
- * Nazeem's test
  */
 public class ExampleTest extends InstrumentationTestCase {
     public void test() throws Exception {
