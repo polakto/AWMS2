e5f54cac151d74c80dfbe74f28777f717f07fbf2
polakto <polakto@gmail.com>
2015-12-10 19:49:19 +0300

    new addTransActivity design

--- app/src/main/java/com/support/android/designlibdemo/AddTransactionActivity.java
+++ app/src/main/java/com/support/android/designlibdemo/AddTransactionActivity.java
@@ -34,4 +34,5 @@ public class AddTransactionActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
+
 }
--- app/src/main/java/data/FirstDataLayer.java
+++ app/src/main/java/data/FirstDataLayer.java
@@ -4,6 +4,7 @@ import interfaces.Data2ToData1;
 
 /**
  * Created by Dell on 24.11.2015.
+ * ABANDONED - DON´T USE THIS CLASS ANYMORE!!!
  */
 public class FirstDataLayer implements Data2ToData1 {
  //body of class
